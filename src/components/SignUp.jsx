1c9ff8d16eef9a65a995f72b0163b9bfa5a40923
immortalWebDev <pgbadgujar007@gmail.com>
2024-05-25 14:49:43 +0530

    Added forgot password link logic

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,19 +1,20 @@
-import React, { useState ,useContext} from "react";
+import React, { useState, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import "./SignUp.css";
 import { AuthContext } from "./AuthContext";
 
 function SignUp() {
-
-    const {handleLogin} = useContext(AuthContext)
+  const { handleLogin } = useContext(AuthContext);
+  const navigate = useNavigate(); // React Router hook to navigate to different routes
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
   const [isLogin, setIsLogin] = useState(false);
-  const navigate = useNavigate(); // React Router hook to navigate to different routes
+  const [forgotPasswordLoading, setForgotPasswordLoading] = useState(false);
+
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -39,32 +40,48 @@ function SignUp() {
         returnSecureToken: true,
       });
 
-      const data = response.data
-      
-      localStorage.setItem('token',data.idToken)
-      localStorage.setItem('userEmail',data.email)
+      const data = response.data;
 
-      handleLogin()
-      
+      localStorage.setItem("token", data.idToken);
+      localStorage.setItem("userEmail", data.email);
+
+      handleLogin();
 
       console.log("Authentication successful:", response.data);
       setError(null);
       setEmail("");
       setPassword("");
       setConfirmPassword("");
-       
-       navigate("/home"); // Navigate to the Home component on successful login/signup
-       
-    
 
+      navigate("/home"); // Navigate to the Home component on successful login/signup
     } catch (err) {
       let errorMessage = "Authentication failed: Invalid credentials";
-    //   if (err.response && err.response.data && err.response.data.error && err.response.data.error.message) {
-    //     errorMessage = err.response.data.error.message;
-    //   }
+      //   if (err.response && err.response.data && err.response.data.error && err.response.data.error.message) {
+      //     errorMessage = err.response.data.error.message;
+      //   }
       setError(errorMessage);
     } finally {
       setIsLoading(false);
+    }
+  };
+
+  const handleForgotPassword = async () => {
+    setForgotPasswordLoading(true)
+    try {
+      await axios.post(
+        `https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=AIzaSyDxRZQnGCbIbLdX0T-hMudwZE9GiRmWIIw`,
+        {
+          requestType: "PASSWORD_RESET",
+          email: email,
+        }
+      );
+      console.log("Password reset email sent successfully!");
+      setError('Password reset email sent successfully!');
+    } catch (error) {
+      console.error("Error sending password reset email:", error);
+      setError("Error sending password reset email. Please enter valid email.");
+    } finally {
+      setForgotPasswordLoading(false)
     }
   };
 
@@ -102,14 +119,25 @@ function SignUp() {
         )}
         {error && <p className="error-message">{error}</p>}
         <button type="submit" disabled={isLoading}>
-          {isLoading ? "Processing..." : isLogin ? "Log in" : "Sign up"}
+          {isLoading ? "Logging in..." : isLogin ? "Log in" : "Sign up"}
         </button>
+        {isLogin && (
+          <button
+            type="button"
+            onClick={handleForgotPassword}
+            disabled={isLoading}
+          >
+            {forgotPasswordLoading ? "Sending link..." : "Forgot Password?"}
+          </button>
+        )}
+
         <div className="login-link">
           {isLogin ? "Don't have an account ?" : "Already have an account ?"}
           <button type="button" className="small-button" onClick={toggleLogin}>
             {isLogin ? "Create a new account" : "Log in with existing account"}
           </button>
         </div>
+       
       </form>
     </section>
   );
