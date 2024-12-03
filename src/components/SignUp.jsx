fc69a2c20d0e601902701e0fd1b267835bf13b26
immortalWebDev <pgbadgujar007@gmail.com>
2024-12-03 18:43:38 +0530

    Updated code. Fixed styles, urls exposure etc

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,16 +1,13 @@
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
-import { useNavigate,useLocation } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import axios from "axios";
 import { login } from "../store/authSlice";
 import { useTheme } from "../store/ThemeContext";
 import "./SignUp.css";
 
 const SignUp = () => {
-
-  const {theme} = useTheme()
-  // const location = useLocation();
-  // const message = location.state?.message
+  const { theme } = useTheme();
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -32,11 +29,14 @@ const SignUp = () => {
 
     setIsLoading(true);
 
+    console.log(`${import.meta.env.VITE_FB_SIGNIN}${import.meta.env.VITE_FB_API}`)
+    
+
     let url;
     if (isLogin) {
-      url = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDxRZQnGCbIbLdX0T-hMudwZE9GiRmWIIw`;
+      url = `${import.meta.env.VITE_FB_SIGNIN}${import.meta.env.VITE_FB_API}`;
     } else {
-      url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDxRZQnGCbIbLdX0T-hMudwZE9GiRmWIIw`;
+      url = `${import.meta.env.VITE_FB_SIGNUP}${import.meta.env.VITE_FB_API}`;
     }
 
     try {
@@ -47,16 +47,14 @@ const SignUp = () => {
       });
 
       const data = response.data;
-      
-      
-      console.log(data)
+
+      console.log("Data fetched from server upon successfull auth", data);
 
       // Dispatch login action with user's email
       dispatch(login({ email: data.email, token: data.idToken }));
 
-
-    localStorage.setItem('refreshToken', data.refreshToken); // Store refresh token
-    localStorage.setItem('tokenExpiry', Date.now() + data.expiresIn * 1000);
+      localStorage.setItem("refreshToken", data.refreshToken); // Store refresh token
+      localStorage.setItem("tokenExpiry", Date.now() + data.expiresIn * 1000);
 
       setError(null);
       setEmail("");
@@ -64,6 +62,8 @@ const SignUp = () => {
       setConfirmPassword("");
 
       navigate("/home");
+      window.location.reload(); //to show theme button
+
     } catch (err) {
       setError("Authentication failed: Invalid credentials");
     } finally {
@@ -75,7 +75,7 @@ const SignUp = () => {
     setForgotPasswordLoading(true);
     try {
       await axios.post(
-        `https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=AIzaSyDxRZQnGCbIbLdX0T-hMudwZE9GiRmWIIw`,
+        `${import.meta.env.VITE_FB_PASSWORD_RESET}${import.meta.env.VITE_FB_API}`,
         {
           requestType: "PASSWORD_RESET",
           email: email,
@@ -96,56 +96,60 @@ const SignUp = () => {
 
   return (
     <div>
-    <div className={`signup-container ${theme}`}>
-      <form className="signup-form" onSubmit={handleSubmit}>
-        <h2>{isLogin ? "Login" : "Sign Up"}</h2>
-        <input
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-        {!isLogin && (
+      <div className={`signup-container ${theme}`}>
+        <form className="signup-form" onSubmit={handleSubmit}>
+          <h2>{isLogin ? "Login" : "Sign Up"}</h2>
           <input
-            type="password"
-            placeholder="Confirm Password"
-            value={confirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
             required
           />
-        )}
-        {error && <p className="error-message">{error}</p>}
-        <button type="submit" disabled={isLoading}>
-          {isLoading ? "Loading..." : isLogin ? "Log in" : "Sign up"}
-        </button>
-        {isLogin && (
-          <button
-            type="button"
-            onClick={handleForgotPassword}
-            disabled={forgotPasswordLoading}
-          >
-            {forgotPasswordLoading ? "Sending link..." : "Forgot Password?"}
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+          {!isLogin && (
+            <input
+              type="password"
+              placeholder="Confirm Password"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              required
+            />
+          )}
+          {error && <p className="error-message">{error}</p>}
+          <button type="submit" disabled={isLoading}>
+            {isLoading ? "Loading..." : isLogin ? "Log in" : "Sign up"}
           </button>
-        )}
-        <div className="login-link">
-          {isLogin ? "Don't have an account?" : "Already have an account?"}
-          <button type="button" className="small-button" onClick={toggleLogin}>
-            {isLogin ? "Create a new account" : "Log in with existing account"}
-          </button>
-        </div>
-      </form>
-    </div>
-    <div>
-    {/* {message && <p>{message}</p>} */}
-    </div>
+          {isLogin && (
+            <button
+              type="button"
+              onClick={handleForgotPassword}
+              disabled={forgotPasswordLoading}
+            >
+              {forgotPasswordLoading ? "Sending link..." : "Forgot Password?"}
+            </button>
+          )}
+          <div className="login-link">
+            {isLogin ? "Don't have an account?" : "Already have an account?"}
+            <button
+              type="button"
+              className="small-button"
+              onClick={toggleLogin}
+            >
+              {isLogin
+                ? "Create a new account"
+                : "Log in with existing account"}
+            </button>
+          </div>
+        </form>
+      </div>
+      <div></div>
     </div>
   );
 };
