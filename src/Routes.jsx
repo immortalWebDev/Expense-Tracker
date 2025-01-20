d89fed8d57af25872223e46a8b3999bd906719e0
immortalWebDev <pgbadgujar007@gmail.com>
2025-01-20 15:58:48 +0530

    Fixed routing bug

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -3,18 +3,31 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import SignUp from "./components/SignUp";
 import Home from "./components/Home";
 import ExpenseTracker from "./components/ExpenseTracker";
-import PrivateRoute from "./components/PrivateRoute";
 import { useSelector } from "react-redux";
 
 const AppRoutes = () => {
-  const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
 
   return (
     <Routes>
-      <Route path="/home" element={<PrivateRoute element={Home} />} />
+      <Route
+        path="/home"
+        element={isAuthenticated ? <Home /> : <Navigate to="/signup" />}
+      />
+
       <Route path="/signup" element={<SignUp />} />
-      <Route path="/expenses" element={<PrivateRoute element={ExpenseTracker} />} />
-      <Route path="*" element={<Navigate to={isAuthenticated ? "/home" : "/signup"} />} />
+
+      <Route
+        path="/expenses"
+        element={
+          isAuthenticated ? <ExpenseTracker /> : <Navigate to="/signup" />
+        }
+      />
+
+      <Route
+        path="*"
+        element={<Navigate to={isAuthenticated ? "/home" : "/signup"} />}
+      />
     </Routes>
   );
 };
