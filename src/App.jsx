7ccdb60d8f5ed662981552e6710ed64e3442a48e
immortalWebDev <pgbadgujar007@gmail.com>
2025-01-20 15:42:14 +0530

    Updatinf the theme and bg part (WEP)

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,26 @@ import "./App.css";
 import { useDispatch, useSelector } from "react-redux";
 import { setIsAuthenticated } from "./store/authSlice";
 import { ThemeProvider, useTheme } from "./store/ThemeContext";
-import ThemeToggle from "./components/ThemeToggle";
+// import ThemeToggle from "./components/ThemeToggle";
 import Routes from './Routes'
-import { fetchExpenses } from "./store/expensesSlice";
+// import { fetchExpenses } from "./store/expensesSlice";
+import Navbar from "./components/Navbar";
+// import Footer from "./components/Footer";
 
 const App = () => {
   const dispatch = useDispatch();
   const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
   const { theme } = useTheme();
-  const totalAmount = useSelector(state => state.expenses.totalAmount);
+  // const totalAmount = useSelector(state => state.expenses.totalAmount);
 
   useEffect(() => {
     const token = localStorage.getItem('token');
     dispatch(setIsAuthenticated(!!token)); // Double negation
   }, [dispatch]);
 
-  useEffect(() => {
-    dispatch(fetchExpenses()); //to persist amount state, (to maintain toggle btn)
-  },[])
+  // useEffect(() => {
+  //   dispatch(fetchExpenses()); //to persist amount state, (to maintain toggle btn)
+  // },[])
   
 
   
@@ -29,39 +31,16 @@ const App = () => {
   return (
     <div className={`App ${theme}`}>
       <header className={`App-header ${theme}`}>
-        <nav className="navbar">
-          <div className="navbar-left">
-            <div className="logo">ExpenseEagle!</div>
-            <ul>
-              <li>
-                <NavLink to="/home" end activeclassname="active">
-                  Home
-                </NavLink>
-              </li>
-              <li>
-                <a href="#products">Products</a>
-              </li>
-              <li>
-                <a href="#about">About Us</a>
-              </li>
-              <li>
-                {isAuthenticated && totalAmount >= 10000 && (
-                  <ul>
-                    <ThemeToggle />
-                  </ul>
-                )}
-              </li>
-            </ul>
-          </div>
-        </nav>
+       <Navbar/>
       </header>
       <main className={theme}>
         {isAuthenticated === null ? (
           <div>Loading...</div>
         ) : (
-          <Routes /> // Render the routes component
+          <Routes /> 
         )}
       </main>
+      {/* <Footer></Footer> */}
     </div>
   );
 };
