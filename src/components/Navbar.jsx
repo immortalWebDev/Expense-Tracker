77c882b3cf28f4da5eff1a848aacae801da94cca
immortalWebDev <pgbadgujar007@gmail.com>
2025-01-20 15:47:37 +0530

    Separated navbar from Home (Concerns)

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,29 @@
+import { NavLink } from "react-router-dom";
+import Logout from "./Logout";
+import { useSelector } from "react-redux";
+import "./Navbar.css";
+
+const Navbar = () => {
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+
+  return (
+    <nav className="navbar">
+      <div className="navbar-left">
+        <div className="logo">
+          <NavLink to="/home">
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/2344/2344132.png"
+              alt="logo"
+              width="25px"
+            />{" "}
+            ExpenseEagle!
+          </NavLink>
+        </div>
+
+        {isAuthenticated && <Logout />}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
